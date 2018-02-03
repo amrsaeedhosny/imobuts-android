b3f7b7f49d38c4cef8f98d8a556090b97fabb1a1
amrshosny <amrshosny@gmail.com>
2018-02-03 21:21:48 +0200

    Adding tab layout

--- app/src/main/java/com/example/amrshosny/imobuts/AccountContentActivity.java
+++ app/src/main/java/com/example/amrshosny/imobuts/AccountContentActivity.java
@@ -1,13 +1,33 @@
 package com.example.amrshosny.imobuts;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AccountContentActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_content);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+
+        adapter.addFragment(new TicketsFragment(),null);
+        adapter.addFragment(new ProfileFragment(), null);
+
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ticket);
+        tabLayout.getTabAt(1).setIcon(R.drawable.profile);
+
     }
 }
---
+++ app/src/main/java/com/example/amrshosny/imobuts/TicketsFragment.java
@@ -0,0 +1,24 @@
+package com.example.amrshosny.imobuts;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TicketsFragment extends Fragment {
+    public TicketsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tickets, container, false);
+    }
+}
